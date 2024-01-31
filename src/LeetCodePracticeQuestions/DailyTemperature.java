af0d58be3c70fbad44f0dc324b14ea0ff3d961c4
Navneet Shandilya <30992372+NAVNEET72159@users.noreply.github.com>
2024-01-31 09:26:35 +0530

    commit

---
+++ src/LeetCodePracticeQuestions/DailyTemperature.java
@@ -0,0 +1,25 @@
+package LeetCodePracticeQuestions;
+
+public class DailyTemperature {
+    public static void main(String[] args) {
+        int[] T = {73,74,75,71,69,72,76,73};
+        int[] ans = dailyTemperatures(T);
+        for(int i=0;i<ans.length;i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        for(int i=0;i<temperatures.length;i++) {
+            while(top != -1 && temperatures[stack[top]] < temperatures[i]) {
+                ans[stack[top]] = i - stack[top];
+                top--;
+            }
+            stack[++top] = i;
+        }
+        return ans;
+    }
+}
