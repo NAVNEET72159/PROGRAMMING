1c10a9d003552dc124a3df77d8b3f27ad4c36c63
Navneet Shandilya <30992372+NAVNEET72159@users.noreply.github.com>
2024-02-02 21:51:20 +0530

    commit

---
+++ src/LeetCodePracticeQuestions/SequentialDigits.java
@@ -0,0 +1,25 @@
+package LeetCodePracticeQuestions;
+
+import java.util.List;
+
+public class SequentialDigits {
+    public static void main(String[] args) {
+        int low = 100;
+        int high = 300;
+        System.out.println(sequentialDigits(low, high));
+    }
+    public static List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> result = new java.util.ArrayList<>();
+        for(int i = 1; i <= 9; i++) {
+            int num = i;
+            for(int j = i+1; j <= 9; j++) {
+                num = num*10 + j;
+                if(num >= low && num <= high) {
+                    result.add(num);
+                }
+            }
+        }
+        result.sort(java.util.Comparator.naturalOrder());
+        return result;
+    }
+}
