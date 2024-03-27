52a97e2e7d9f32835b729865cc83c7cb667478cf
Navneet Shandilya <30992372+NAVNEET72159@users.noreply.github.com>
2024-03-27 10:14:08 +0530

    commit

--- src/LeetCodePracticeQuestions/FirstMissingPositive.java
+++ src/LeetCodePracticeQuestions/FirstMissingPositive.java
@@ -2,7 +2,7 @@ package LeetCodePracticeQuestions;
 
 public class FirstMissingPositive {
     public static void main(String[] args) {
-        int[] nums = [1, 2, 0];
+        int[] nums = {1, 2, 0};
         System.out.println(firstMissingPositive(nums));
     }
     public static int firstMissingPositive(int[] nums) {
